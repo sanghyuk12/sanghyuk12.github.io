dd8c9197cd56c28ec4394480bcd66102606bc4ad
Sanghyuk Kim <51909632+sanghyuk12@users.noreply.github.com>
2022-08-12 16:51:35 +0900

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,6 @@
+const metaConfig = require("./stie-meta-config")
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
-    },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
-    },
-  },
+  siteMetadata: metaConfig,
   plugins: [
     `gatsby-plugin-image`,
     {
---
+++ stie-meta-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    title: `HYEOK's SoftWare`,
+    subtitle: ``,
+    description: ``,
+    author: {
+        name: `@SANG-HYEOK KIM`,
+        summary: `Seoul, Korea`,
+    },
+    introduction: `상혁의 블로그입니다.`,
+    siteUrl: `https://github.com/sanghyuk12`, // Your blog site url
+    social: {
+        github: `https://github.com/sanghyuk12`, // Your GitHub account
+        medium: ``, // Your Medium account
+        facebook: `01023541045/`, // Your Facebook account
+        linkedin: ``, // Your LinkedIn account
+        instagram: `01023541045`,
+    },
+    icon: ``, // Add your favicon
+    keywords: [
+        `blog`,
+        `gatsby`,
+        `application`,
+        `react`,
+        `FontEnd`,
+        `Developer`,
+        `HYEOK's SoftWare`,
+    ],
+    comment: {
+        utterances: 'HYEOK\'s SoftWare', // Your repository for archive comment
+    },
+}
